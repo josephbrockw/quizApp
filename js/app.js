176ea8e78b37f6ca1aa4325cc52fe8d313d9270f
josephbrockw <joew@theroasterie.com>
2015-03-25 18:55:23 -0500

    I need help

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var listQ = ["points", "shooter", "attendance", "win", "playoff"]
 	var questions = {
 		points: ["Who led The Bulls in points for the 2007-2008 season?", "Loul Deng", "Ben Gordon", "Kirk Hinrich", "Joakim Noah"]
-		shooter: ["Who is The Bulls all-time leader in 3pt field goals made?", "Michael Jordan", "Steve Kerr", "Kirk Hinrich", "Toni Kukoc"]
+		//shooter: ["Who is The Bulls all-time leader in 3pt field goals made?", "Michael Jordan", "Steve Kerr", "Kirk Hinrich", "Toni Kukoc"]
 		//attendance: ["How many consecutive years has The Bulls led the NBA in attendance?", "3", "4", "5", "6"]
 		//win: ["What is The Bulls' all-time win percentage?", ".448", ".490", ".515", ".522"]
 		//playoff: ["How many play off appearances have The Bulls made?", "20", "26", "33", "42"]
